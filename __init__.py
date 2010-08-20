ded7071020d8b300c9bd07af21e6084a812ca1ea
Ariel Núñez <ingenieroariel@gmail.com>
2010-08-20 16:58:23 -0500

    Added basic files for a Django project

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__about__ = """
+RHoK Planet helps with the management of projects/events/people in the Random
+Hacks of Kindness events.
+"""
+
---
+++ urls.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+
+from django.contrib import admin
+admin.autodiscover()
+
+
+urlpatterns = patterns("",
+    url(r"^$", direct_to_template, {
+        "template": "homepage.html",
+    }, name="home"),
+    url(r"^admin/", include(admin.site.urls)),
+)
+
+
+if settings.SERVE_MEDIA:
+    urlpatterns += patterns("",
+        url(r"", include("staticfiles.urls")),
+    )
